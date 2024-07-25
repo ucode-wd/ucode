e7ee67f7cbf5e0d9ffb1f6466919061ada24044d
ucode-wd <162057735+ucode-wd@users.noreply.github.com>
2024-07-25 12:06:54 -0300

    Delete src directory

--- src/components/NosotrosSection.jsx
+++
@@ -1,9 +0,0 @@
-import './styles/nosotros.css'
-export default function NosotrosSection(){
-    return(
-        <section className="nosotros" id="nosotros">
-            <h1>ucode</h1>
-            <h3>Diseño y desarrollo web</h3>
-        </section>
-    )
-}
