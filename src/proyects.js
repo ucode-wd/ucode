65a39ed9588c9bbad50da3283fa774be0ce0c6a0
Luca Tittarelli <lucatittarelli06@gmail.com>
2025-11-10 11:14:14 -0300

    Add astro version

---
+++ src/proyects.js
@@ -0,0 +1,45 @@
+const proyectsData = [
+    {
+        titulo: "Comuna Villa Elisa",
+        imagen: "villa-elisa.webp",
+        parrafo: "Sitio institucional para la comuna, con integración de un sistema de gestión de cuotas.",
+        url: "comunavillaelisa.gob.ar"
+    },
+    {
+        titulo: "Catálogo de Partes",
+        imagen: "catalogodepartes.webp",
+        parrafo: `
+            Sistema digital para la gestión y visualización de repuestos/productos. 
+            Incluye catálogo interactivo, buscador avanzado y módulo de pedidos en línea. 
+            Ejemplos: <br/>
+            <a href="https://ombu.catalogodepartes.com" target="_blank" class="text-indigo-400">Ombú</a> | 
+            <a href="https://rotulastassi.catalogodepartes.com" target="_blank" class="text-indigo-400">Tassi</a>.
+        `,
+        url: "catalogodepartes.com"
+    },
+    {
+        titulo: "Meyer Automotores",
+        imagen: "meyer.webp",
+        parrafo: "Plataforma de exhibición de vehículos para concesionaria, con panel administrativo para carga y gestión de datos.",
+        url: "meyerautomotores.com.ar"
+    },
+    {
+        titulo: "Consultorios Mayo",
+        imagen: "consultorios-mayo.webp",
+        parrafo: "Landing page informativa para un centro de atención médica.",
+        url: "consultoriosmayo.com.ar"
+    },
+    // {
+    //     titulo: "Tittarelli Mecanizados",
+    //     imagen: "tittarelli-mecanizados.webp",
+    //     parrafo: "Landing page corporativa para un taller de mecanizados y servicios metalúrgicos.",
+    //     url: "tittarellimecanizados"
+    // },
+    {
+        titulo: "Ms-Tech",
+        imagen: "ms-tech.webp",
+        parrafo: "Sitio web profesional para una empresa dedicada a soluciones tecnológicas y servicios IT.",
+        url: "ms-tech.com.ar"
+    }
+];
+export default proyectsData;
